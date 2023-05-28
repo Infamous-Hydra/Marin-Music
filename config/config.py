354b302df1d543c259d8f1725033a8918df41bd0
Karma <116187035+Infamous-Hydra@users.noreply.github.com>
2023-05-28 13:49:16 +0600

    Update config.py

--- config/config.py
+++ config/config.py
@@ -45,7 +45,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Infamous-Hydra/MikoMusic",
+    "https://github.com/Infamous-Hydra/Miko-Music",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
